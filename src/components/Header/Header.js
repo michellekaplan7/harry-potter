15fb59b1796379311e9d2c7841f5c1df7765d17f
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-06-04 13:16:33 -0600

    :crystal_ball: Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+
+    return (
+        <div className="header-container">
+            <h1>WIZARD HARRY</h1>
+            <img className="logo" src='./images/wizard-harry-logo.jpg' alt="wizard harry logo" /> 
+        </div>
+    )
+}
+
+export default Header
