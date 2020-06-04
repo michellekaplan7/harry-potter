6df89c9bc2d6d3872c865765f7002df325dbedd5
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-06-04 10:48:20 -0600

    :crystal_ball: Add link that wraps a button on the landing page that goes to the explore path

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Landing.css";
 
 const Landing = () => {
-
-    return (
-        <div>
-            Helloooo
-        </div>
-
-    )
-}
+  return (
+    <div>
+      <Link className="landing-link" to="/explore">
+        <button className="landing-button">I Solemnly Swear That I Am Up To No Good</button>
+      </Link>
+    </div>
+  );
+};
 
 export default Landing;
