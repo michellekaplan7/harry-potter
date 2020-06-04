6983d251c2b1b071b8f00161ce66a91885b811d9
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-06-04 17:30:49 -0600

    :crystal_ball: Keep state in RandomHouse component and have a handleClick method that fetches a random house from API and conditionally render images and audio

--- src/components/RandomHouse/RandomHouse.js
+++ src/components/RandomHouse/RandomHouse.js
@@ -1,55 +1,51 @@
-// import React, { Component } from "react";
-// import "./RandomHouse.css";
-// import { getRandomHouse } from "../../apiCalls";
-
-// class RandomHouse extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       house: "",
-//       images: ['test']
-//     };
-//   }
-
-
-//   handleClick = async () => {
-//     const house = await getRandomHouse();
-//     this.setState({ house });
-//   };
-
-//   render() {
-//     return (
-//       <div className="sorting-container">
-//         {/* <img className="house-logo" alt="house logo" src={this.state.house === "gryffindor" ? this.state.images[0]: null}  /> */}
-//         <button className="sort-button" onClick={() => this.handleClick()}>Get House</button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default RandomHouse;
-
-import React from "react";
+import React, { Component } from "react";
 import "./RandomHouse.css";
+import { getRandomHouse } from "../../apiCalls";
 
-const Home = (props) => {
+class RandomHouse extends Component {
+  constructor() {
+    super();
+    this.state = {
+      house: "",
+    };
+  }
 
-    const pictures = [
-        "/images/gryffindor.jpg",
-        "/images/hufflepuff.jpg",
-        "/images/ravenclaw.jpg",
-        "/images/slytherin.jpg",
-        "/images/sorting_hat.jpg"
-	    ];
+  handleClick = async () => {
+    const house = await getRandomHouse();
+    this.setState({ house });
+  };
+
+  render() {
+    const houseName = this.state.house.toLowerCase();
 
     return (
-        <div className="sorting-container">
-            <h2 className="sorting-message">Let the sorting begin!</h2>
-            <img className="house-logo" alt="house logo" src={props.randomHouse === "gryffindor" ? pictures[0]: props.randomHouse === "hufflepuff" ? pictures[1]: props.randomHouse === "ravenclaw" ? pictures[2] : props.randomHouse === "slytherin" ? pictures[3] : pictures[4]}  />
-            <audio src={props.randomHouse === "gryffindor" ? "/audio/gryffindor.m4a" : props.randomHouse === "hufflepuff" ? "/audio/hufflepuff.m4a" : props.randomHouse === "ravenclaw" ? "/audio/ravenclaw.m4a" : props.randomHouse === "slytherin" ? "/audio/slytherin.m4a" : null } autoPlay  />
-            <button className="sort-button" onClick={() => props.getRandomHouse()}>Get sorted!</button>
-        </div>
-    )
+      <div className="sorting-container">
+        {this.state.house && <h3>{this.state.house.toUpperCase()}!</h3>}
+
+        {this.state.house ? (
+          <img
+            className="house-logo"
+            alt="house logo"
+            src={`/images/${houseName}.jpg`}
+          />
+        ) : (
+          <img
+            className="harry-potter-cartoon"
+            alt="harry-potter-cartoon"
+            src={"/images/harry-potter-cartoon.jpg"}
+          />
+        )}
+
+        {this.state.house ? (
+          <audio src={`/audio/${houseName}.m4a`} autoPlay />
+        ) : null}
+
+        <button className="sort-button" onClick={() => this.handleClick()}>
+          Get House
+        </button>
+      </div>
+    );
+  }
 }
 
-export default Home;
+export default RandomHouse;
