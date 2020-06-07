6de8f0a61e70a491ef6b211b40a05800001d471a
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-06-07 13:20:51 -0600

    :crystal_ball: Add CharactersContainer component and add initial skeleton

---
+++ src/components/CharactersContainer/CharactersContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./CharactersContainer.css";
+
+class CharactersContainer extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    render() {
+
+        return (
+            <div className="characters-container">
+                HELLOOOO
+            </div>
+        )
+    }
+}
+
+export default CharactersContainer
