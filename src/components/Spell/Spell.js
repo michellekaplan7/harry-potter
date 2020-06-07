6496cb156eee7402476a5a9a7b0e4fc2c14a8261
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-06-07 12:44:50 -0600

    :crystal_ball: Add star icon and conditional rendering of star based on favorite status and add in toggleFavorites method to the star

--- src/components/Spell/Spell.js
+++ src/components/Spell/Spell.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import './Spell.css';
+import React from "react";
+import "./Spell.css";
+import MdStar from "react-ionicons/lib/MdStar";
+import MdStarOutline from "react-ionicons/lib/MdStarOutline";
 
-const Spell = ({ id, effect, spell, type }) => {
+const Spell = ({ id, effect, spell, type, toggleFavorites, favorite }) => {
+    console.log('fav', favorite)
+  return (
+    <div className="spell-container" id={id}>
+      <h4>Spell: {spell}</h4>
+      <h4>Effect: {effect}</h4>
+      <h4>Type: {type}</h4>
 
-    return (
-        <div className="spell-container" data-testid={id} >
-            <h4>Spell: {spell}</h4>
-            <h4>Effect: {effect}</h4>
-            <h4>Type: {type}</h4>
-        </div>
-    )
-}
+      {favorite ? (
+        <MdStar
+          color="yellow"
+          fontSize="60px"
+        //   className="spell-remove-fav"
+          onClick={() => toggleFavorites(id)}
+        />
+      ) : (
+        <MdStarOutline
+          color="white"
+          fontSize="60px"
+        //   className="spot-add-fav"
+          onClick={() => toggleFavorites(id)}
+        />
+      )}
+    </div>
+  );
+};
 
-export default Spell
+export default Spell;
