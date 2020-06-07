bb5980817e9e70764e2243795d82c62032249000
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-06-07 16:42:53 -0600

    :bar_chart: Fix formatting of Landing test file

--- src/components/Landing/Landing.test.js
+++ src/components/Landing/Landing.test.js
@@ -5,16 +5,16 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/";
 
 describe("Landing", () => {
-    it("should display the correct information", () => {
-        const router = (
-            <MemoryRouter>
-                <Landing />
-            </MemoryRouter>
-        );
+  it("should display the correct information", () => {
+    const router = (
+      <MemoryRouter>
+        <Landing />
+      </MemoryRouter>
+    );
 
-        const { getByRole } = render(router);
-        const link = getByRole("link", { name: "I Solemnly Swear That I Am Up To No Good" });
+    const { getByRole } = render(router);
+    const link = getByRole("link", {name: "I Solemnly Swear That I Am Up To No Good"});
 
-        expect(link).toBeInTheDocument();
-    });
-})
+    expect(link).toBeInTheDocument();
+  });
+});
