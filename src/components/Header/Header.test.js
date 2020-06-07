0a74449fcb87a3adc0564ba0f56129dbfc08d175
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-06-07 16:36:01 -0600

    :bar_chart: Add unit test for Header component

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Header";
+import { MemoryRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/";
+
+describe("Header", () => {
+    it("should display the correct information", () => {
+        const router = (
+            <MemoryRouter>
+                <Header />
+            </MemoryRouter>
+        );
+
+        const { getByText, getByAltText } = render(router);
+        const headerText = getByText("WIZARD HARRY");
+        const logo = getByAltText("wizard harry logo")
+
+        expect(headerText).toBeInTheDocument();
+        expect(logo).toBeInTheDocument();
+    });
+})
