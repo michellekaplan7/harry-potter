d20b1c0801a9e7ae4159154b925a132f0c0f9e02
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-06-07 15:45:35 -0600

    :crystal_ball: Add api fetch call for fetching character data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,8 @@
-
 export const getRandomHouse = async () => {
-    const response = await fetch('https://www.potterapi.com/v1/sortingHat')
-    const houseData = await response.json()
-    return houseData
-}
-
+  const response = await fetch("https://www.potterapi.com/v1/sortingHat");
+  const houseData = await response.json();
+  return houseData;
+};
 
 // export const getAdvice = async (setError, setIsLoading, setAdvice) => {
 //     const url = 'https://api.adviceslip.com/advice'
@@ -20,35 +18,61 @@ export const getRandomHouse = async () => {
 //     setIsLoading(false)
 //   }
 
+export const getAdvice = async () => {
+  const url = "https://api.adviceslip.com/advice";
+  //   setError('')
 
-  export const getAdvice = async () => {
-    const url = 'https://api.adviceslip.com/advice'
-    //   setError('')
-        
-      try {
-        const response = await fetch(url, {
-          cache: "no-cache"
-        })
-        const advice = await response.json()
-        return advice
-        // setAdvice(advice.slip.advice)
-      } catch(error) {
-        // setError(error.message)
-      }
-    //   setIsLoading(false)
-    }
+  try {
+    const response = await fetch(url, {
+      cache: "no-cache",
+    });
+    const advice = await response.json();
+    return advice;
+    // setAdvice(advice.slip.advice)
+  } catch (error) {
+    // setError(error.message)
+  }
+  //   setIsLoading(false)
+};
 
+export const getSpells = async () => {
+  const url =
+    "https://www.potterapi.com/v1/spells?key=$2a$10$qrr46GleJRJDlrmt/wH4lubKz67jvDjOwTjpDGqCcXoV8MX9i5qNC";
 
-    export const getSpells = async () => {
-        const url = 'https://www.potterapi.com/v1/spells?key=$2a$10$qrr46GleJRJDlrmt/wH4lubKz67jvDjOwTjpDGqCcXoV8MX9i5qNC'
-            
-          try {
-            const response = await fetch(url)
-            const spellsData = await response.json()
-            return spellsData
-          } catch(error) {
-            console.log(error.message)
-          }
-        }
+  try {
+    const response = await fetch(url);
+    const spellsData = await response.json();
+    return spellsData;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getCharacters = async () => {
+  const url =
+    "https://www.potterapi.com/v1/characters?key=$2a$10$qrr46GleJRJDlrmt/wH4lubKz67jvDjOwTjpDGqCcXoV8MX9i5qNC";
+
+  try {
+    const response = await fetch(url);
+    const charactersData = await response.json();
+    const promises = charactersData.map(character => {
+      return {
+        id: character._id,
+        name:character.name,
+        role:character.role,
+        house:character.house,
+        ministryOfMagic:character.ministryOfMagic,
+        orderOfThePhoenix:character.orderOfThePhoenix,
+        dumbledoresArmy:character.dumbledoresArmy,
+        deathEater:character.deathEater,
+        bloodStatus:character.bloodStatus,
+        species: character.species
+      };
+    })
+    return Promise.all(promises)
 
     
+  } catch (error) {
+    console.log(error.message);
+  }
+};
