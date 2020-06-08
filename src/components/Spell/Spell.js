5a81d74bd883c7667e2098c73bf73b1cb82e4aef
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-06-08 11:08:51 -0600

    :crystal_ball: Remove console logs and format document

--- src/components/Spell/Spell.js
+++ src/components/Spell/Spell.js
@@ -4,7 +4,6 @@ import MdStar from "react-ionicons/lib/MdStar";
 import MdStarOutline from "react-ionicons/lib/MdStarOutline";
 
 const Spell = ({ id, effect, spell, type, toggleFavorites, favorite }) => {
-    console.log('fav', favorite)
   return (
     <div className="spell-container" id={id}>
       <h4>Spell: {spell}</h4>
@@ -15,14 +14,12 @@ const Spell = ({ id, effect, spell, type, toggleFavorites, favorite }) => {
         <MdStar
           color="yellow"
           fontSize="60px"
-        //   className="spell-remove-fav"
           onClick={() => toggleFavorites(id)}
         />
       ) : (
         <MdStarOutline
           color="white"
           fontSize="60px"
-        //   className="spot-add-fav"
           onClick={() => toggleFavorites(id)}
         />
       )}
