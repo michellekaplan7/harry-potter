0beb0e5d94a108e31e91a71d35e535526fc64489
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-06-08 22:03:37 -0600

    :crystal_ball: Add propTypes

--- src/components/Spell/Spell.js
+++ src/components/Spell/Spell.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Spell.css";
+import PropTypes from "prop-types";
 import MdStar from "react-ionicons/lib/MdStar";
 import MdStarOutline from "react-ionicons/lib/MdStarOutline";
 
 const Spell = ({ id, effect, spell, type, toggleFavorites, favorite }) => {
   return (
     <div className="spell-container" id={id}>
-      <h4 className="spell-info"><span>Spell:</span> {spell}</h4>
-      <h4 className="spell-info"><span>Effect:</span> {effect}</h4>
-      <h4 className="spell-info"><span>Type:</span> {type}</h4>
+      <h4 className="spell-info">
+        <span>Spell:</span> {spell}
+      </h4>
+      <h4 className="spell-info">
+        <span>Effect:</span> {effect}
+      </h4>
+      <h4 className="spell-info">
+        <span>Type:</span> {type}
+      </h4>
 
       {favorite ? (
         <div data-testid={`fullStar-${id}`}>
@@ -29,6 +36,15 @@ const Spell = ({ id, effect, spell, type, toggleFavorites, favorite }) => {
       )}
     </div>
   );
+};
+
+Spell.propTypes = {
+  id: PropTypes.string,
+  effect: PropTypes.string,
+  spell: PropTypes.string,
+  type: PropTypes.string,
+  favorite: PropTypes.bool,
+  toggleFavorites: PropTypes.func,
 };
 
 export default Spell;
